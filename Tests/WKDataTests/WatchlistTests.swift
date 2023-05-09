79ecf089a5fd46f562f872c81351a2b8ca844c6c
Toni Sevener <tsevener@wikimedia.org>
2023-05-09 09:12:51 -0500

    Revert "Make more public"
    This reverts commit 7fc0a5d8e2af27e8b73308b76147bbda1df3d375.

--- Tests/WKDataTests/WatchlistTests.swift
+++ Tests/WKDataTests/WatchlistTests.swift
@@ -1,13 +1,13 @@
 import XCTest
 @testable import WKData
 
-public class MockWatchlistFetcher: WatchlistFetching {
-    public let languageCodes: [String]
-    public init(languageCodes: [String]) {
+private class MockWatchlistFetcher: WatchlistFetching {
+    let languageCodes: [String]
+    init(languageCodes: [String]) {
         self.languageCodes = languageCodes
     }
     
-    public func fetchWatchlist(completion: (Result<WKData.WatchlistAPIResponse, Error>) -> Void) {
+    func fetchWatchlist(completion: (Result<WKData.WatchlistAPIResponse, Error>) -> Void) {
         let item1 = WatchlistAPIResponse.Query.Item(title: "Item 1")
         let item2 = WatchlistAPIResponse.Query.Item(title: "Item 2")
         let item3 = WatchlistAPIResponse.Query.Item(title: "Item 3")
