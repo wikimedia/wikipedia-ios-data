25367fd07a3e37c3e0590fcd121e61586eed2564
Toni Sevener <tsevener@wikimedia.org>
2023-05-09 11:31:01 -0500

    Still more

--- Sources/WKData/WatchlistFetching.swift
+++ Sources/WKData/WatchlistFetching.swift
@@ -5,17 +5,25 @@ public struct WatchlistAPIResponse: Codable {
     public struct Query: Codable {
         
         public struct Item: Codable {
+            public let title: String
+            
             public init(title: String) {
                 self.title = title
             }
-            
-            public let title: String
         }
         
         public let watchlist: [Item]
+        
+        public init(watchlist: [Item]) {
+            self.watchlist = watchlist
+        }
     }
     
     public let query: Query
+    
+    public init(query: Query) {
+        self.query = query
+    }
 }
 
 
