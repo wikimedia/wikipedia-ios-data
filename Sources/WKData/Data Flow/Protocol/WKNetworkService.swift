ea1637595874aba114735b3ce0bc6c34632f9d44
Deepak Mantena <staykids@users.noreply.github.com>
2023-05-12 10:50:24 -0700

    Update protocol.

--- Sources/WKData/Data Flow/Protocol/WKNetworkService.swift
+++ Sources/WKData/Data Flow/Protocol/WKNetworkService.swift
@@ -1,5 +1,5 @@
 import Foundation
 
 public protocol WKNetworkService {
-	func perform(request: WKNetworkRequest, completion: @escaping (Result<String, Error>) -> Void)
+	func perform(request: WKNetworkRequest, completion: @escaping (Result<[String: Any]?, Error>) -> Void)
 }
