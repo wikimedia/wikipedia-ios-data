369dee101620ac5812e01e402360e39b622a4b5c
Toni Sevener <tsevener@wikimedia.org>
2023-06-02 16:39:26 -0500

    Add app languages to WKAppData

--- Sources/WKData/Environment/WKDataEnvironment.swift
+++ Sources/WKData/Environment/WKDataEnvironment.swift
@@ -1,9 +1,17 @@
 import Foundation
 
+public struct WKAppData {
+    let appLanguages: [WKLanguage]
+    
+    public init(appLanguages: [WKLanguage]) {
+        self.appLanguages = appLanguages
+    }
+}
+
 public final class WKDataEnvironment: ObservableObject {
 
 	public static let current = WKDataEnvironment()
 
-	@Published public private(set) var mediaWikiNetworkService: WKNetworkService?
-
+    @Published public var appData = WKAppData(appLanguages: [])
+    @Published public var mediaWikiNetworkService: WKNetworkService?
 }
