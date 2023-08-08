c99a2d05b8091b06d346dd4893b2229f0af01102
Toni Sevener <tsevener@wikimedia.org>
2023-08-08 18:06:54 -0500

    Add WKKeyValueStore and conforming WKUserDefaultsStore

--- Sources/WKData/Environment/WKDataEnvironment.swift
+++ Sources/WKData/Environment/WKDataEnvironment.swift
@@ -13,5 +13,7 @@ public final class WKDataEnvironment: ObservableObject {
 	public static let current = WKDataEnvironment()
 
     @Published public var appData = WKAppData(appLanguages: [])
-    @Published public var mediaWikiService: WKService?
+    public var mediaWikiService: WKService?
+    public var userDefaultsStore: WKKeyValueStore? = WKUserDefaultsStore()
+    public var sharedCacheStore: WKKeyValueStore?
 }
--- Sources/WKData/Errors.swift
+++ Sources/WKData/Errors.swift
@@ -1,10 +1,23 @@
 import Foundation
 
+// MARK: Common
+
 public enum WKDataControllerError: Error {
     case mediaWikiServiceUnavailable
-    case appLanguagesUnavailable
     case failureCreatingRequestURL
     case unexpectedResponse
     case serviceError(Error)
     case mediaWikiResponseError(WKMediaWikiError)
+}
+
+public enum WKUserDefaultsStoreError: Error {
+    case unexpectedType
+    case failureDecodingJSON(Error)
+    case failureEncodingJSON(Error)
+}
+
+// MARK: Feature-specific
+
+public enum WKWatchlistError: Error {
+    case failureDeterminingProjects
 }
