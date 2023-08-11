40ed53c6389cd54947fd8cf80d0aa57cb51a3604
Toni Sevener <tsevener@wikimedia.org>
2023-08-11 16:48:55 -0500

    Make public init for WKMockKeyValueStore for use in Components tests

--- Sources/WKDataMocks/WKMockKeyValueStore.swift
+++ Sources/WKDataMocks/WKMockKeyValueStore.swift
@@ -2,6 +2,11 @@ import Foundation
 import WKData
 
 public final class WKMockKeyValueStore: WKKeyValueStore {
+    
+    public init() {
+        self.savedObjects = [:]
+    }
+    
     private var savedObjects: [String: Codable] = [:]
     public func load<T>(key: String...) throws -> T? where T : Decodable, T : Encodable {
         let defaultsKey = key.joined(separator: ".")
